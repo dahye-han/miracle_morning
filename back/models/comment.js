33b3d0f614f6a664085ade795f51a70b0cb79a3b
Hanae <doithanae@gmail.com>
2021-03-27 20:57:14 +0900

    0327

--- back/models/comment.js
+++ back/models/comment.js
@@ -1,17 +1,27 @@
-module.exports = (sequelize, DataTypes) => {
-    const Comment = sequelize.define('Comment', { //MySQL에는 users 테이블 생성
-        // id가 기본적으로 들어있다.
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Comment extends Model {
+  static init(sequelize) {
+    return super.init({
+      // id가 기본적으로 들어있다.
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      // UserId: 1
+      // PostId: 3
     }, {
-        charset: 'utf8mb4', 
-        collate: 'utf8mb4_general_ci', //한글+이모티콘
+      modelName: 'Comment',
+      tableName: 'comments',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci', // 이모티콘 저장
+      sequelize,
     });
-    Comment.associate = (db) => {
-        db.Comment.belongsTo(db.User);
-        db.Comment.belongsTo(db.Post);
-    };
-    return Comment;
-}
+  }
+
+  static associate(db) {
+    db.Comment.belongsTo(db.User);
+    db.Comment.belongsTo(db.Post);
+  }
+};
--- back/models/image.js
+++ back/models/image.js
@@ -1,16 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-    const Image = sequelize.define('Image', { //MySQL에는 users 테이블 생성
-        // id가 기본적으로 들어있다.
-        src: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-        },
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Image extends Model {
+  static init(sequelize) {
+    return super.init({
+      // id가 기본적으로 들어있다.
+      src: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
     }, {
-        charset: 'utf8', 
-        collate: 'utf8_general_ci', //한글+이모티콘
+      modelName: 'Image',
+      tableName: 'images',
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+      sequelize,
     });
-    Image.associate = (db) => {
-        db.Image.belongsTo(db.Post);
-    };
-    return Image;
-}
+  }
+  static associate(db) {
+    db.Image.belongsTo(db.Post);
+  }
+};
